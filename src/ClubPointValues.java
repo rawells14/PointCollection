22e333c69cf10bba935b36da650a915e7f675633
Ryan Wells <rawells14@gmail.com>
2014-09-24 13:58:22 -0500

    GUI Changes
    Added Default Values Class
    Added User Info Page

---
+++ src/ClubPointValues.java
@@ -0,0 +1,24 @@
+
+public class ClubPointValues {
+
+    public int tshirt = 1;
+    public int meetingAttendance = 1;
+    public int contest = 3;
+    public int foodDonation = 1;
+
+    public int getTshirt() {
+        return tshirt;
+    }
+
+    public int getMeetingAttendance() {
+        return meetingAttendance;
+    }
+
+    public int getContest() {
+        return contest;
+    }
+
+    public int getFoodDonation() {
+        return foodDonation;
+    }
+}
--- src/gui.java
+++ src/gui.java
@@ -8,16 +8,24 @@ public class gui extends javax.swing.JFrame {
     public gui() {
         initComponents();
         jPanel2.setVisible(false);
+        jCheckBox1.setVisible(false);
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+        jTextField1 = new javax.swing.JTextField();
+        jComboBox1 = new javax.swing.JComboBox();
+        jCheckBox1 = new javax.swing.JCheckBox();
+        jTextField2 = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jButton3 = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -34,6 +42,59 @@ public class gui extends javax.swing.JFrame {
         setMinimumSize(new java.awt.Dimension(397, 300));
         getContentPane().setLayout(null);
 
+        jPanel2.setBackground(new java.awt.Color(153, 153, 153));
+        jPanel2.setLayout(null);
+
+        jLabel2.setText("User Info");
+        jPanel2.add(jLabel2);
+        jLabel2.setBounds(160, 20, 70, 14);
+
+        jButton2.setText("Sign In");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton2);
+        jButton2.setBounds(150, 220, 90, 23);
+
+        jTextField1.setText("Full Name");
+        jPanel2.add(jTextField1);
+        jTextField1.setBounds(150, 40, 80, 30);
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Meeting", "Contest", "Volunteer" }));
+        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox1ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jComboBox1);
+        jComboBox1.setBounds(150, 80, 80, 20);
+
+        jCheckBox1.setText("TShirt");
+        jPanel2.add(jCheckBox1);
+        jCheckBox1.setBounds(150, 110, 90, 23);
+
+        jTextField2.setText("Amount Donated");
+        jPanel2.add(jTextField2);
+        jTextField2.setBounds(150, 110, 130, 20);
+
+        getContentPane().add(jPanel2);
+        jPanel2.setBounds(0, 0, 400, 310);
+
+        jLabel1.setText("Point Collection");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(150, 40, 99, 14);
+
+        jButton3.setText("Help");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton3);
+        jButton3.setBounds(300, 260, 80, 23);
+
         jButton1.setText("Check In");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -41,49 +102,49 @@ public class gui extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton1);
-        jButton1.setBounds(145, 257, 75, 23);
-
-        jLabel1.setText("Point Collection");
-        getContentPane().add(jLabel1);
-        jLabel1.setBounds(145, 20, 99, 14);
-
-        jPanel2.setBackground(new java.awt.Color(153, 153, 153));
-
-        jLabel2.setText("hello");
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(56, Short.MAX_VALUE)
-                .addComponent(jLabel2)
-                .addGap(54, 54, 54))
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(19, 19, 19)
-                .addComponent(jLabel2)
-                .addContainerGap(166, Short.MAX_VALUE))
-        );
-
-        getContentPane().add(jPanel2);
-        jPanel2.setBounds(122, 40, 132, 199);
+        jButton1.setBounds(130, 260, 110, 23);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        jPanel2.setVisible(true);
+        jTextField2.setVisible(false);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         try {
             PrintWriter pw = new PrintWriter(new FileWriter("data.csv", true));
-            jPanel2.setVisible(true);
+
             pw.append("\nhello,hello");
             pw.append("");
             pw.close();
         } catch (Exception e) {
         }
-    }//GEN-LAST:event_jButton1ActionPerformed
+        addPoints();
+        reset();
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+        jCheckBox1.setVisible(false);
+        jTextField2.setVisible(false);
+        if (jComboBox1.getSelectedIndex() == 0) {
+            jCheckBox1.setVisible(true);
+        }
+        if (jComboBox1.getSelectedIndex() == 1) {
+            jTextField2.setVisible(true);
+        }
+    }//GEN-LAST:event_jComboBox1ActionPerformed
+    public void reset() {
+        jCheckBox1.setSelected(false);
+        jTextField2.setText("Amount Donated");
+        jComboBox1.setSelectedIndex(0);
+        jPanel2.setVisible(false);
+    }
 
     public void addPoints() {
     }
@@ -98,9 +159,15 @@ public class gui extends javax.swing.JFrame {
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JCheckBox jCheckBox1;
+    private javax.swing.JComboBox jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
 }
